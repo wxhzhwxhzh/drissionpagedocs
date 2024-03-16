164376c9a19ee30f50e3885652b5b40950b3a204
wxhzhwxhzh <gaoqingdianying@qq.com>
2024-03-16 23:35:23 +0800

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -16,7 +16,7 @@ const config = {
   url: 'https://g1879.gitee.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/drissionpagedocs/',
+  baseUrl: '/SaossionPageDocs/build/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
